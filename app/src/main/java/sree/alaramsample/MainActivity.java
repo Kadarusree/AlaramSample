da5db9b0287aec9d690165e506a5bc14cc34dd0a
srikanthk <ksreekanthyadav@gmail.com>
2018-10-09 16:48:42 +0530

    Added Google Sign In

--- app/src/main/java/sree/alaramsample/MainActivity.java
+++ app/src/main/java/sree/alaramsample/MainActivity.java
@@ -1,7 +1,19 @@
 package sree.alaramsample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -11,8 +23,9 @@ import sree.alaramsample.alarm_calendar.AlarmDBHelper;
 import sree.alaramsample.alarm_calendar.AlarmManagerHelper;
 import sree.alaramsample.alarm_calendar.AlarmModel;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public AlarmDBHelper dbHelper_calendar = new AlarmDBHelper(this);
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addAlarm();
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
     }
 
 
@@ -118,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
             alarmDetails.isEnabled = true;
             dbHelper_calendar.createAlarm(alarmDetails);
         }
-		/*
+        /*
 		 * alarmDetails.setRepeatingDay(AlarmModel.SUNDAY, true);
 		 * alarmDetails.setRepeatingDay(AlarmModel.MONDAY, true);
 		 * alarmDetails.setRepeatingDay(AlarmModel.TUESDAY, true);
@@ -133,6 +159,49 @@ public class MainActivity extends AppCompatActivity {
 
         if (dbHelper_calendar.getAlarms() != null && dbHelper_calendar.getAlarms().size() > 0) {
             AlarmManagerHelper.setAlarms(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            // ...
+        }
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 1);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 1) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Toast.makeText(getApplicationContext(), account.getDisplayName(), Toast.LENGTH_LONG).show();
+            // Signed in successfully, show authenticated UI.
+            //    updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("", "signInResult:failed code=" + e.getStatusCode());
+            // updateUI(null);
         }
     }
 }
